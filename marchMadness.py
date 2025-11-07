0f59990587e3d295b8f9de657ff678de02e6596a
mgm <garciamelomariano@gmail.com>
2025-11-06 18:01:22 -0600

    Initial commit

---
+++ marchMadness.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split, KFold
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score, f1_score
+from sklearn.preprocessing import StandardScaler
+from sklearn.svm import SVC
