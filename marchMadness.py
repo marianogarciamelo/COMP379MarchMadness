f02fbfa86ac082b3e661db02880ec3a17cd39187
Nathan Johnson <nathanjohnson482@gmail.com>
2025-11-13 12:52:51 -0600

    Added feature_builder which combines specified CSV

--- marchMadness.py
+++ marchMadness.py
@@ -5,3 +5,8 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score, f1_score
 from sklearn.preprocessing import StandardScaler
 from sklearn.svm import SVC
+from feature_builder import materialize_dataset
+
+
+if __name__ == "__main__":
+    materialize_dataset()
